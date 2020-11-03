e99d153a3b970f3f8f1259fd2ff32c5d419d7ba6
Andersraae <andersraae01@gmail.com>
2020-11-03 10:16:30 +0100

    En halv Insertion

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -21,11 +21,21 @@ def bogoSort(items):
     print('Sorteret efter {} forsøg'.format(attempts))
     return items
 
+def InerstionSort(items):
+    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
+    items = items.copy()
+    i = 0
+    for j in items:
+        if items[i] < items[i-1]:
+            gem = items[i-1]
+            items[i-1]=items[i]
+            items[i] = gem
+
 
 if __name__ == '__main__':
     for i in range(10):
         listen = list(range(1, 8))
-        sorteret = bogoSort(listen)
+        sorteret = InerstionSort(listen)
         random.shuffle(listen)
         print('Shuffled:\t', listen)
         print('Sorted:\t\t', sorteret)
