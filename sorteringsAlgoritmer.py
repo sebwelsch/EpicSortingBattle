3a4b0674690b3f93c729e8e32aef21c680f92c3b
Andersraae <andersraae01@gmail.com>
2020-11-17 19:04:31 +0100

    Det var nok alt for idag

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,25 +1,27 @@
 import random, math, sys
 
-def bogoSort(items):
-    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
-    items = items.copy()
-    isSorted = None # Boolean til markering af, om listen er sorteret
-    attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
-    while not isSorted:
-        attempts += 1
-        if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
-            print('Afbryder på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
-            items.sort()
-            return items
-        random.shuffle(items) # Bland alle elementer helt tilfældigt
-        isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
-        # ...og prøver i denne løkke at bevise det modsatte
-        for index in range(len(items)-1):
-            if items[index] > items[index+1]:
-                isSorted = False
-                break # Bryd løkken hvis et eneste element er forkert sorteret
-    print('Sorteret efter {} forsøg'.format(attempts))
-    return items
+
+# def bogoSort(items):
+#     # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
+#     items = items.copy()
+#     isSorted = None # Boolean til markering af, om listen er sorteret
+#     attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
+#     while not isSorted:
+#         attempts += 1
+#         if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
+#             print('Afbryder på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
+#             items.sort()
+#             return items
+#         random.shuffle(items) # Bland alle elementer helt tilfældigt
+#         isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
+#         # ...og prøver i denne løkke at bevise det modsatte
+#         for index in range(len(items)-1):
+#             if items[index] > items[index+1]:
+#                 isSorted = False
+#                 break # Bryd løkken hvis et eneste element er forkert sorteret
+#     print('Sorteret efter {} forsøg'.format(attempts))
+#     return items
+
 
 def InsertionSort(items):
     # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
@@ -43,16 +45,16 @@ def SelectionSort(items):
     return items
 
 
-def MergeSort(items):
-    items = items.copy()
-    holder = [None]
-    for i in range(0,len(items)):
-        if i % len(items)/2 == 0:
-            for j in range(0,int(len(items)/2)):
-                holder.append(items[j])
-                print(holder)
-            if holder < len(items)+1: #+1 fordi første værdi er None
-                items[len(items)]
+# def MergeSort(items):
+#     items = items.copy()
+#     holder = [None]
+#     for i in range(0,len(items)):
+#         if i % len(items)/2 == 0:
+#             for j in range(0,int(len(items)/2)):
+#                 holder.append(items[j])
+#                 print(holder)
+#             if holder < len(items)+1: #+1 fordi første værdi er None
+#                 items[len(items)]
 
 
 
